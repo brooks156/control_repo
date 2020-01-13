430bcc25f3336a6ca1079d2d4b856fb17c18f08f
brooks156 <kathy.brooks@gmail.com>
2020-01-13 14:33:49 -0700

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
     ensure  => present,
     user    => 'root',
     type    => 'ssh-rsa',
-    key 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDmvD3zmkUM6kmIz+CWhkAiQt8gfXRSYuAb48NAf6ENgoqXqY8AXRyY+n+1e4UDCbhrXJ/1fLNk9XkB9ysiCMjFFRanLJ9pZq5q1+BQ8J5TQmF5zIDAs+FCceS+UmPPXa5dilLcJabyAXKncIznZjfiNurS0d5cAABriEwYRbKrQe/oFtfeLawROy64JvEjLAUl7tVGVpO3pXqOlglGsaouHfE2Ae1nlKlADFTMk6h4GDK72LOSfxvHctrSRTD0uURoyx1q9jfNcgeWNBKvmaA0+vZmv9mzIqrDch9icmCRcrNeFwx13PnDqPtuAdA3cfEjRGS6AVLxF5v6dGFz8JFT',
+    key     => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDmvD3zmkUM6kmIz+CWhkAiQt8gfXRSYuAb48NAf6ENgoqXqY8AXRyY+n+1e4UDCbhrXJ/1fLNk9XkB9ysiCMjFFRanLJ9pZq5q1+BQ8J5TQmF5zIDAs+FCceS+UmPPXa5dilLcJabyAXKncIznZjfiNurS0d5cAABriEwYRbKrQe/oFtfeLawROy64JvEjLAUl7tVGVpO3pXqOlglGsaouHfE2Ae1nlKlADFTMk6h4GDK72LOSfxvHctrSRTD0uURoyx1q9jfNcgeWNBKvmaA0+vZmv9mzIqrDch9icmCRcrNeFwx13PnDqPtuAdA3cfEjRGS6AVLxF5v6dGFz8JFT',
     }
   }
